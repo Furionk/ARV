f71728af6b8b2bbd179fc9a5f628b13a2d6b9a8b
Furion Mashiou <mgi_179@hotmail.com>
2018-01-12 02:23:23 +0800

    2018 1 11 (2)

---
+++ Assets/Sources/Components/VehiclePartComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Entitas;
+using Entitas.CodeGeneration.Attributes;
+
+public class VehiclePartComponent : IComponent {
+
+    public string Name;
+    public Vector3 Position;
+
+}
